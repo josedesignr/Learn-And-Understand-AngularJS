e2acb61a4488ac67a18386285cd8255ba2c5833d
Alvaro José Solis <alvarojose827@gmail.com>
2016-11-04 13:33:52 -0500

    Global Namespace

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,14 +1,32 @@
 
 
-/* 
-AngularJS is an MVC (Model-View-Controller) framework.
-The MODEL is here (JS), the VIEW, is basically what the user can see (HTML), and the CONTROLLER is what binds MODEL and VIEw.
+/* THE GLOBAL NAMESPACE */
 
-Whatever happens in the View, affects the Model; and viceversa.
-
-
+/*
+	Imagine that this place (where you are reading/writing), is a space in the computer memory
+	that is at the very top of everything, the general space, the main space.
+	Everything that you define here, will be visible in any other function that you creates, because is being
+	defined outside, not enclosed in anything, just in the Global Namespace. 
 */
 
-//-- Using jQuery, I am printing the value stored in data-my-attribute. Still not using AngularJS.
-//-- Read the explanation of attributes in the index.html, so we can move forward.
-console.log($("h1").attr("data-my-attribute"));
+//-- Let's create a variable in the Global Namespace.
+var person = "Jose";
+
+//-- And then printing it in the console...
+console.log(person);
+
+/*
+WARNING: This is just an example, but it is NOT correct to define variables in the Global Namespace.
+
+...Why?
+Because in real scenarios, you are going to use different modules, frameworks, libraries, somebody else's code, etc.
+And if you have such a bad luck, and define a variable with the same name that other variable in any other place of the project,
+it is going to be overwritten, if person exists twice, only the last variable person is going to be stored.
+
+And the first one is just going to be replaced.
+This can cause serious problems and bugs, and not so easy to find.
+
+In the next lesson, we are going to learn how to avoid polluting the Global Namespace, with Local Namespaces.
+
+Go to the next tag with GIT. 
+*/
