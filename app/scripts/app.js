fbe1e57ce46735c82e2fb146084784085a6235df
Alvaro José Solis <alvarojose827@gmail.com>
2016-11-06 23:49:32 -0500

    JavaScript Aside: The Event Loop

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,45 +1,43 @@
-/* NG-MODEL AND TWO-WAY DATA BINDING */
-
-/*
-	It's time to get a handle on one of, if not the most important topic
-	when it comes to learning and understanding AngularJS: Directives, and Two-Way Data Binding.
-
-	If you have heard or read anything abot AngularJS, you probably have heard the term "directive",
-	It's used all the time, and it sounds really complex, but it really isn't as complicated as it sounds.
-
-	IMPORTANT CONCEPT:
-	Directive: An instruction to AngularJS to manipulate a piece of the DOM.
-	(this could be "add a class", 'hide this', 'create this', etc...)
-	There are a lot of directives that AngularJS has created for us, we already used two of them:
-	ng-controller and ng-app, but there are much more.
-	* ng-class: to add or remove a class from an element depending on some condition.
-	* ng-show: shows an element if a condition is met.
-	* ng-hide: hides an element if a condition is met.
-	* ng-if: removes an element from the DOM is a condition is not met.
-	* ng-repeat: iterates across an array or object (like a for), so it creates as much elements as an array has, but in the HTML you just need to create one pair of tags.
-	* ng-model: binds data from here to the html, and any change made here or in the html, changes also in the other side
-
-	There are much more directives, but ng-model is one of the most powerful ones, and that is the one we are going to use in this lesson.
-
-	If you have done a little bit of JavaScript or jQuery you may have manually changed what was going on in the DOM,
-	(Document Object Model) that is a representation in memory of the HTML.
-
-	AngularJS prefers that we use directives, because it makes it much more powerful and flexible.
-*/
-
 var app = angular.module('myApp', []);
 
 
 app.controller('mainController', ['$scope', '$filter', function($scope, $filter) {
 
-	//--First we create a property in the $scope, which starts empty.
-	$scope.account = "";
-
-	//--This is not necessary, it's just to show how we can also call functions in a interpolation in the HTML. (See there)
-	$scope.accountInLowercase = function(){
-
-		//--Here we are passing whatever value of $scope.account, and converting it to lowercase.
-		return $filter('lowercase')($scope.account);
-	};
-
 }]);
+
+
+//-- Forget about this AngularJS code, this will be another JavaScript Aside, just pure JS.
+
+
+/* JAVASCRIPT ASIDE: THE EVENT LOOP */
+
+/*
+	We are going to talk about the event loop. and how events are happening
+	and working inside JavaScript.
+	And that will help us understand how AngularJS takes advange of them.
+*/
+
+//-- This is pure JavaScript...
+
+//--Let's create a variable and say that we want to look for an element with ID "name"
+var myVar = document.getElementById("name");
+
+//--This console is just to test if it was found.
+console.log(myVar);
+
+
+/* 
+	Now, we are going to create something called "Event Listener", which basically,
+	as its name says, is listening if some specific event is thrown; in this case,
+	it will be listening every time we press a key on the "name" input.
+	And when it happens, will trigger the function, which in this case is just a console.log.
+	No only once, but every time that the event occurs, the function gets invoked.
+	That is the Event Loop.
+
+	With JavaScript or jQuery, you have to listen for events manually (as it is here),
+	AngularJS on the other hand, takes advantage on those events to keep track of things for you.
+*/
+myVar.addEventListener("keypress", function(event){
+	console.log("Pressed!");
+});
+
