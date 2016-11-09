122855ebc8f1a1d3cac31f96afbaccff7fed7399
Alvaro José Solis <alvarojose827@gmail.com>
2016-11-09 11:40:17 -0500

    Concept of Singletons

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,78 +1,67 @@
-/* ROUTING, TEMPLATES AND CONTROLLERS */
+/* SINGLETONS AND SERVICES */
 
 /*
-	The way AngularJS constructs Single Page Applications involves several
-	fundamental concepts and we are going to discuss those now.
+	When you hear the term Angular Services, you immediately hear the term "Singletons".
+	SINGLETON:
+	The one and ONLY copy of an object, it's a pattern in object oriented programming, and it means
+	that I only have one of that object, ever.
+
+	For example: We might in the code have a person object and then, elsewhere in the code
+	instanciate or create copies of that person object (like a Jose object, Steve object, etc).
+
+	BUT, with a singleton, there is ONLY ONE.
+	whenever you ask for the object, you are never getting a copy, you are getting the one and only object.
+
+	AngularJS services are implemented as singletons. Let's prove that:
 */
 
 var app = angular.module('myApp', ['ngRoute']);
 
-app.controller('mainController', ['$scope', '$location', '$log', function($scope, $location, $log) {
-
-	//--Remember what we did in last lesson to get the hash? The Angular service $location does the same.
-	$log.info($location.path());
-
-	/* 
-		This console.log is just to let you know that Angular already has a way to get the hash
-		and since the hash is a key part of its structure, it also has a module that allows us
-		to define which view should be shown depending on the route (fragment identifier).
-
-		This module is ngRoute, which doesn't come in the AngularJS core, so we need to download it
-		from angularjs.org, import it in the HTML (see there), and inject it in the main module ('myApp')
-	*/
-}]);
-
-//-- Once the ngRoute is injected in the 'myApp' module. HOW DOES IT WORK?
-
-//--First, we have to set a config, which receives a function, which receives a $routeProvider.
+//-- CONFIG ROUTES
 app.config(function($routeProvider) {
-
-	//--$routeProvider watches the hash, in order to search matches with the routes created by us.
 	$routeProvider
-
-	//--The when() method has the route that we create, and an object with certain properties.
-	.when('/', {
-
-		/*  One of the properties is templateUrl, which in other words is saying:
-			When my website is www.myapp.com/#/ show the HTML that is stored in views/home.html
+		.when('/', {
+			templateUrl: 'views/home.html',
+			controller: 'mainController'
+		})
+		.when('/second', {
+			templateUrl: 'views/second.html',
+			controller: 'secondController'
+		})
+		/* This is something I forgot to include in last lesson
+			the otherwise() method, is used by the routeProvier
+			when none of the previous routes matches.
+			Normally, the otherwise, goes to the '/' or home. 
 		*/
-		templateUrl: 'views/home.html',
-		
-		/*  Another property is controller, that sets which controller binds to the view
-			that we defined above (home.html)
-			This is the same as putting an ng-controller at the very first tag of home.html
-			But it's a best practice to set the controller here.
-		*/
-		controller: 'mainController'
-	})
-	//-- We can create as many routes as we want, setting a view and a controller for each one.
-	//-- For example, when my route is www.myapp.com/#/second, is going to show second.html
-	.when('/second', {
-		templateUrl: 'views/second.html',
-		controller: 'secondController'
-	})
-
-	/*  This works very well but... What happen if we don't know exactly which route would it be?
-
-		For example, what if the hash is not going to be a constant, but it's going to depend
-		on the logged user?
-		www.myapp.com/#/alvarojose when Alvaro Jose is logs in,
-		but www.myapp.com/#/johndoe when Jhon Doe logs in.
-
-		To resolve that, we can also define routes with parameters. Let's see how it works.
-	*/
-	//--By putting : and a parameter name (in this case username), we say that username is variable.
-	.when('/second/:username', {
-		templateUrl: 'views/second.html',
-		controller: 'secondController'
-	})
+		.otherwise('/');
 });
 
-//--Now let's create the secondController, and since we have a route with parameters, we must inject $routeParams
-app.controller('secondController', ['$scope', '$routeParams', function($scope, $routeParams){
+//--CONTROLLERS
+app.controller('mainController', ['$scope', '$log', function($scope, $log) {
 
-	//-- Here we're extracting the parameter from the url, and giving that value to a variable in the $scope.
-	//-- The || 'none' is optional, it means that if there's no parameter, but only www.myapp.com/second then set the variable to 'none'
-	$scope.username = $routeParams.username || 'none';
+	//-- If we add a property to the $log service.
+	$log.myProperty = "Property from Main Controller";
 
+	$log.log($log);
 }]);
+
+
+app.controller('secondController', ['$scope', '$log', function($scope, $log){
+
+	//--And we add another property to the $log service from this controller...
+	$log.anotherProperty = "Property from Second Controller";
+
+	$log.log($log);
+}]);
+
+/*
+	We can enter to both pages and check the logs, you will see that both properties are added to the same $log object.
+
+	But, wait a minute...
+	If all the Angular Services are singletons and all the created properties goes to the same singleton.
+
+	HOW IS POSIBLE THAT $scope WORKS IN THE WAY IT DOES?
+	Well, that is an exception. $scope is the only Angular module that is not a Singleton,
+	actually the Singleton is $rootScope and the different $scope across the app, are childs.
+*/
+
