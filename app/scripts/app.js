2cd3b7758a7812f7bd90c00e1425862039fb41cf
Alvaro José Solis <alvarojose827@gmail.com>
2016-11-11 10:33:21 -0500

    Rendering Custom Directives with ng-repeat

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,4 +1,12 @@
-/* @, =, AND OTHER OBTUSE SYMBOLS: PART 3 */
+/* RENDERING CUSTOM DIRECTIVES WITH ng-repeat */
+
+/*
+	This lesson is very short, here we just changed the person object to be an array of persons.
+	We defined it in a service, so we do not have to repeat the same in both controllers.
+	Afterwards, we injected our service into the controllers to be able to use the data.
+	And lastly, we implemented ng-repeat in our directive (see home.html and second.html),
+	in order to iterate over each person a render a different person with each iteration.
+*/
 
 var app = angular.module('myApp', ['ngRoute']);
 
@@ -16,29 +24,41 @@ app.config(function($routeProvider) {
 		.otherwise('/');
 });
 
-
+//--SERVICES:
 /*
-	This is very important, it could be a bit confusing but I'll try  to explain in detail.
-
-	As we saw in last lesson, a Directive is a piece of HTML that we put in an independant file,
-	in order to be reused in several places, no matter controllers or in which view you place it.
-
-	This is a great advantage, but it also can be dangerous because, since the Directive is a
-	piece o HTML that is placed at any place that we want to put it, you perfectly can put the
-	same directive in two different views (each with a different controller), and it's valid.
-
-	The problem comes when, for example, you put the same directive in 2 different views,
-	like, putting <search-result></search-result> in home.html ('/') and second.html ('/second')
-	Those two views have their own controller ($scope), and, since the directive (a piece oh HTML)
-	is inside the view, it can also access the controller of the view where the directive is included.
-
-	That can be great, but if the directive is going to do things to the items and elements on the
-	scope, it's going to set changes in the parent controller. (the view where the directive is placed)
-	And this, perhaps, can cause an unexpected and unwished behavior.
-
-	AngularJS provide us a way to avoid this, by giving to the directive its own scope, so it will
-	not mess with the parent scope where the directive is placed. Let's see:
+	This service was created in order to avoid repeating the same array and function in both controllers.
 */
+app.service('peopleService', function(){
+	
+	var self = this;
+	self.people = [
+		{
+			name: 'John Doe',
+			address: '555 Main St',
+			city: 'Ney York',
+			state: 'NY',
+			zip: '11111'
+		},
+		{
+			name: 'Jane Doe',
+			address: '26th Roadway',
+			city: 'Miami',
+			state: 'FL',
+			zip: '33166'
+		},
+		{
+			name: 'Alvaro Jose',
+			address: 'Cra 94 # 48-65',
+			city: 'Cali',
+			state: 'CO',
+			zip: '000000'
+		}
+	];
+
+	self.joinAddress = function(person){
+		return person.address + ', ' + person.city + '. ' + person.state + ' ' + person.zip;
+	};
+});
 
 
 //--DIRECTIVES:
@@ -48,19 +68,8 @@ app.directive('searchResult', function(){
 		templateUrl: 'directives/search-result.html',
 		restrict: 'AECM',
 		replace: true,
-		
-		/*  By setting the scope property here into the directive, it will look only in this
-			isolated scope and not to other scopes.
-			So, it doesn't matter where is placed, it won't take the parent's scope, but this one.  
-		*/
 		scope: {
-			//-- The = symbol is explained in the previous lesson.
 			personObject: "=",
-			/*  What if we had a function on the scope of the controller, that is defining the template
-				that contains the directive, and what if we want to have access to it.
-				Well, if we want to access a function that is in the parent scope, we need to use 
-				the & symbol.
-			*/
 			formattedAddress: "&"
 		}	
 	}
@@ -68,43 +77,17 @@ app.directive('searchResult', function(){
 
 
 //--CONTROLLERS
-app.controller('mainController', ['$scope', function($scope) {
+app.controller('mainController', ['$scope', 'peopleService', function($scope, peopleService) {
 	
-	$scope.person = {
-		name: 'John Doe',
-		address: '555 Main St',
-		city: 'Ney York',
-		state: 'NY',
-		zip: '11111'
-	};
-
-	//--Let's suppose that we want to access this function in the isolated scope of the directive 
-	$scope.joinAddress = function(person){
-		return person.address + ', ' + person.city + '. ' + person.state + ' ' + person.zip;
-	};
+	$scope.people = peopleService.people;
+	$scope.joinAddress = peopleService.joinAddress;
+	
 
 }]);
 
-app.controller('secondController', ['$scope', function($scope){
+app.controller('secondController', ['$scope', 'peopleService', function($scope, peopleService){
 
-	$scope.person = {
-		name: 'Jane Doe',
-		address: '26th Roadway',
-		city: 'Miami',
-		state: 'FL',
-		zip: '33166'
-	};
-
-	$scope.joinAddress = function(person){
-		return person.address + ', ' + person.city + '. ' + person.state + ' ' + person.zip;
-	};
+	$scope.people = peopleService.people;
+	$scope.joinAddress = peopleService.joinAddress;
 
 }]);
-
-/*
-	Notice that mainController and secondController, both have the person property, but with 
-	different values, and depending on what view you have selected, it shows the right values.
-
-	The function joinAddress is just the same in both.
-*/
-
